b525b2199a122595d44091ccdeb005d0974e95fe
Austin Rippee <austinrippee@gmail.com>
2025-06-16 11:12:43 -0400

    Add files via upload

---
+++ MeteorDash/Form1.Designer.vb
@@ -0,0 +1,310 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.rock6 = New System.Windows.Forms.PictureBox()
+        Me.rock7 = New System.Windows.Forms.PictureBox()
+        Me.rock5 = New System.Windows.Forms.PictureBox()
+        Me.rock2 = New System.Windows.Forms.PictureBox()
+        Me.rock3 = New System.Windows.Forms.PictureBox()
+        Me.rock4 = New System.Windows.Forms.PictureBox()
+        Me.rock1 = New System.Windows.Forms.PictureBox()
+        Me.pbxSpaceShip = New System.Windows.Forms.PictureBox()
+        Me.timerRock1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblLivesText = New System.Windows.Forms.Label()
+        Me.lblLives = New System.Windows.Forms.Label()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.PictureBox45 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtBoxMovement = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.timerRock2 = New System.Windows.Forms.Timer(Me.components)
+        Me.timerRock3 = New System.Windows.Forms.Timer(Me.components)
+        Me.timerRock4 = New System.Windows.Forms.Timer(Me.components)
+        Me.timerRock5 = New System.Windows.Forms.Timer(Me.components)
+        Me.timerRock6 = New System.Windows.Forms.Timer(Me.components)
+        Me.timerRock7 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.rock6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rock7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rock5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rock2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rock3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rock4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rock1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxSpaceShip, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox45, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'rock6
+        '
+        Me.rock6.Image = Global.MeteorDash.My.Resources.Resources._119498436179961113moon_charles_mccolm_01_svg_hi
+        Me.rock6.Location = New System.Drawing.Point(697, 12)
+        Me.rock6.Name = "rock6"
+        Me.rock6.Size = New System.Drawing.Size(95, 89)
+        Me.rock6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.rock6.TabIndex = 11
+        Me.rock6.TabStop = False
+        '
+        'rock7
+        '
+        Me.rock7.Image = Global.MeteorDash.My.Resources.Resources._119498436179961113moon_charles_mccolm_01_svg_hi
+        Me.rock7.Location = New System.Drawing.Point(828, 12)
+        Me.rock7.Name = "rock7"
+        Me.rock7.Size = New System.Drawing.Size(95, 89)
+        Me.rock7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.rock7.TabIndex = 10
+        Me.rock7.TabStop = False
+        '
+        'rock5
+        '
+        Me.rock5.Image = Global.MeteorDash.My.Resources.Resources._119498436179961113moon_charles_mccolm_01_svg_hi
+        Me.rock5.Location = New System.Drawing.Point(564, 12)
+        Me.rock5.Name = "rock5"
+        Me.rock5.Size = New System.Drawing.Size(95, 89)
+        Me.rock5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.rock5.TabIndex = 8
+        Me.rock5.TabStop = False
+        '
+        'rock2
+        '
+        Me.rock2.Image = Global.MeteorDash.My.Resources.Resources._119498436179961113moon_charles_mccolm_01_svg_hi
+        Me.rock2.Location = New System.Drawing.Point(157, 12)
+        Me.rock2.Name = "rock2"
+        Me.rock2.Size = New System.Drawing.Size(95, 89)
+        Me.rock2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.rock2.TabIndex = 7
+        Me.rock2.TabStop = False
+        '
+        'rock3
+        '
+        Me.rock3.Image = Global.MeteorDash.My.Resources.Resources._119498436179961113moon_charles_mccolm_01_svg_hi
+        Me.rock3.Location = New System.Drawing.Point(284, 12)
+        Me.rock3.Name = "rock3"
+        Me.rock3.Size = New System.Drawing.Size(95, 89)
+        Me.rock3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.rock3.TabIndex = 6
+        Me.rock3.TabStop = False
+        '
+        'rock4
+        '
+        Me.rock4.Image = Global.MeteorDash.My.Resources.Resources._119498436179961113moon_charles_mccolm_01_svg_hi
+        Me.rock4.Location = New System.Drawing.Point(422, 12)
+        Me.rock4.Name = "rock4"
+        Me.rock4.Size = New System.Drawing.Size(95, 89)
+        Me.rock4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.rock4.TabIndex = 5
+        Me.rock4.TabStop = False
+        '
+        'rock1
+        '
+        Me.rock1.Image = Global.MeteorDash.My.Resources.Resources._119498436179961113moon_charles_mccolm_01_svg_hi
+        Me.rock1.Location = New System.Drawing.Point(25, 12)
+        Me.rock1.Name = "rock1"
+        Me.rock1.Size = New System.Drawing.Size(95, 89)
+        Me.rock1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.rock1.TabIndex = 1
+        Me.rock1.TabStop = False
+        '
+        'pbxSpaceShip
+        '
+        Me.pbxSpaceShip.Image = Global.MeteorDash.My.Resources.Resources._0001
+        Me.pbxSpaceShip.Location = New System.Drawing.Point(395, 619)
+        Me.pbxSpaceShip.Name = "pbxSpaceShip"
+        Me.pbxSpaceShip.Size = New System.Drawing.Size(122, 89)
+        Me.pbxSpaceShip.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbxSpaceShip.TabIndex = 0
+        Me.pbxSpaceShip.TabStop = False
+        '
+        'timerRock1
+        '
+        Me.timerRock1.Interval = 250
+        '
+        'lblLivesText
+        '
+        Me.lblLivesText.AutoSize = True
+        Me.lblLivesText.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLivesText.ForeColor = System.Drawing.Color.Red
+        Me.lblLivesText.Location = New System.Drawing.Point(760, 716)
+        Me.lblLivesText.Name = "lblLivesText"
+        Me.lblLivesText.Size = New System.Drawing.Size(92, 31)
+        Me.lblLivesText.TabIndex = 13
+        Me.lblLivesText.Text = "Lives:"
+        '
+        'lblLives
+        '
+        Me.lblLives.AutoSize = True
+        Me.lblLives.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLives.ForeColor = System.Drawing.Color.Red
+        Me.lblLives.Location = New System.Drawing.Point(882, 716)
+        Me.lblLives.Name = "lblLives"
+        Me.lblLives.Size = New System.Drawing.Size(0, 31)
+        Me.lblLives.TabIndex = 14
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlay.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnPlay.Location = New System.Drawing.Point(405, 430)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(107, 54)
+        Me.btnPlay.TabIndex = 15
+        Me.btnPlay.Text = "PLAY"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.lblTitle.Font = New System.Drawing.Font("Bauhaus 93", 39.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblTitle.Location = New System.Drawing.Point(274, 325)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(368, 60)
+        Me.lblTitle.TabIndex = 16
+        Me.lblTitle.Text = "METEOR DASH"
+        '
+        'PictureBox45
+        '
+        Me.PictureBox45.BackColor = System.Drawing.Color.Black
+        Me.PictureBox45.Location = New System.Drawing.Point(-1, -2)
+        Me.PictureBox45.Name = "PictureBox45"
+        Me.PictureBox45.Size = New System.Drawing.Size(20, 827)
+        Me.PictureBox45.TabIndex = 86
+        Me.PictureBox45.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.Location = New System.Drawing.Point(929, -16)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(20, 827)
+        Me.PictureBox1.TabIndex = 87
+        Me.PictureBox1.TabStop = False
+        '
+        'txtBoxMovement
+        '
+        Me.txtBoxMovement.Location = New System.Drawing.Point(1043, 381)
+        Me.txtBoxMovement.Name = "txtBoxMovement"
+        Me.txtBoxMovement.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxMovement.TabIndex = 88
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(-1, 872)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(933, 36)
+        Me.PictureBox2.TabIndex = 89
+        Me.PictureBox2.TabStop = False
+        '
+        'timerRock2
+        '
+        Me.timerRock2.Interval = 250
+        '
+        'timerRock3
+        '
+        Me.timerRock3.Interval = 250
+        '
+        'timerRock4
+        '
+        Me.timerRock4.Interval = 250
+        '
+        'timerRock5
+        '
+        Me.timerRock5.Interval = 250
+        '
+        'timerRock6
+        '
+        Me.timerRock6.Interval = 250
+        '
+        'timerRock7
+        '
+        Me.timerRock7.Interval = 250
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BackgroundImage = Global.MeteorDash.My.Resources.Resources._1
+        Me.ClientSize = New System.Drawing.Size(929, 765)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.txtBoxMovement)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox45)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.lblLives)
+        Me.Controls.Add(Me.lblLivesText)
+        Me.Controls.Add(Me.rock6)
+        Me.Controls.Add(Me.rock7)
+        Me.Controls.Add(Me.rock5)
+        Me.Controls.Add(Me.rock2)
+        Me.Controls.Add(Me.rock3)
+        Me.Controls.Add(Me.rock4)
+        Me.Controls.Add(Me.rock1)
+        Me.Controls.Add(Me.pbxSpaceShip)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.rock6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rock7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rock5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rock2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rock3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rock4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rock1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxSpaceShip, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox45, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents rock1 As System.Windows.Forms.PictureBox
+    Friend WithEvents rock4 As System.Windows.Forms.PictureBox
+    Friend WithEvents rock3 As System.Windows.Forms.PictureBox
+    Friend WithEvents rock2 As System.Windows.Forms.PictureBox
+    Friend WithEvents rock6 As System.Windows.Forms.PictureBox
+    Friend WithEvents rock7 As System.Windows.Forms.PictureBox
+    Friend WithEvents rock5 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbxSpaceShip As System.Windows.Forms.PictureBox
+    Friend WithEvents timerRock1 As System.Windows.Forms.Timer
+    Friend WithEvents lblLivesText As System.Windows.Forms.Label
+    Friend WithEvents lblLives As System.Windows.Forms.Label
+    Friend WithEvents btnPlay As System.Windows.Forms.Button
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents PictureBox45 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents txtBoxMovement As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents timerRock2 As Timer
+    Friend WithEvents timerRock3 As Timer
+    Friend WithEvents timerRock4 As Timer
+    Friend WithEvents timerRock5 As Timer
+    Friend WithEvents timerRock6 As Timer
+    Friend WithEvents timerRock7 As Timer
+End Class
---
+++ MeteorDash/Form1.vb
@@ -0,0 +1,319 @@
+﻿Public Class Form1
+    Protected Overrides Function ProcessCmdKey(ByRef msg As System.Windows.Forms.Message, keyData As System.Windows.Forms.Keys) As Boolean
+        Select Case keyData
+
+            Case Keys.Right
+                pbxSpaceShip.Left += 133
+                If pbxSpaceShip.Left < 0 Then
+                    pbxSpaceShip.Left = 0
+                End If
+                If pbxSpaceShip.Left > 800 Then
+                    pbxSpaceShip.Left = 800
+                End If
+                Return True
+
+
+            Case Keys.Left
+                pbxSpaceShip.Left -= 133
+                If pbxSpaceShip.Left < 0 Then
+                    pbxSpaceShip.Left = 0
+                End If
+                If pbxSpaceShip.Left > 800 Then
+                    pbxSpaceShip.Left = 800
+                End If
+                Return True
+
+        End Select
+
+        Return MyBase.ProcessCmdKey(msg, keyData)
+    End Function
+    Dim rock As Integer
+    Dim speed As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rock1.Visible = False
+        rock2.Visible = False
+        rock3.Visible = False
+        rock4.Visible = False
+        rock5.Visible = False
+        rock6.Visible = False
+        rock7.Visible = False
+        pbxSpaceShip.Visible = False
+        lblLives.Visible = False
+        lblLivesText.Visible = False
+    End Sub
+
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+
+
+        lblTitle.Visible = False
+        btnPlay.Visible = False
+        pbxSpaceShip.Visible = True
+        lblLives.Visible = True
+        lblLivesText.Visible = True
+        lblLives.Text = "3"
+        startRocks()
+
+
+
+    End Sub
+    Private Sub startRocks()
+        'rock 1
+        speed = CInt(Int((200 * Rnd()) + 200))
+        rock1.Visible = True
+        rock1.Top = 0
+        timerRock1.Interval = speed
+        timerRock1.Start()
+
+        'rock 2
+        speed = CInt(Int((200 * Rnd()) + 200))
+        rock2.Visible = True
+        rock2.Top = 0
+        timerRock2.Interval = speed
+        timerRock2.Start()
+        'rock3
+        speed = CInt(Int((200 * Rnd()) + 200))
+        rock3.Visible = True
+        rock3.Top = 0
+        timerRock3.Interval = speed
+        timerRock3.Start()
+        'rock 4
+        speed = CInt(Int((200 * Rnd()) + 200))
+        rock4.Visible = True
+        rock4.Top = 0
+        timerRock4.Interval = speed
+        timerRock4.Start()
+        'rock 5
+        speed = CInt(Int((200 * Rnd()) + 200))
+        rock5.Visible = True
+        rock5.Top = 0
+        timerRock5.Interval = speed
+        timerRock5.Start()
+        'rock 6
+        speed = CInt(Int((200 * Rnd()) + 200))
+        rock6.Visible = True
+        rock6.Top = 0
+        timerRock6.Interval = speed
+        timerRock6.Start()
+        'rock 7
+        speed = CInt(Int((200 * Rnd()) + 200))
+        rock7.Visible = True
+        rock7.Top = 0
+        timerRock7.Interval = speed
+        timerRock7.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles timerRock1.Tick
+        rock1.Top = rock1.Top + 20
+        If rock1.Top >= 800 Then
+            rock1.Top = 0
+            speed = CInt(Int((200 * Rnd()) + 200))
+            timerRock1.Interval = speed
+
+
+        End If
+
+        If rock1.Bounds.IntersectsWith(pbxSpaceShip.Bounds) Then
+            timerRock1.Stop()
+            timerRock2.Stop()
+            timerRock3.Stop()
+            timerRock4.Stop()
+            timerRock5.Stop()
+            timerRock6.Stop()
+            timerRock7.Stop()
+            If lblLives.Text = "3" Then
+                MsgBox("You now have 2 lives remaining")
+                lblLives.Text = "2"
+            ElseIf lblLives.Text = "2" Then
+                MsgBox("You now have 1 life remaining")
+                lblLives.Text = "1"
+            ElseIf lblLives.Text = "1" Then
+                lblLives.Text = "0"
+                MsgBox("You Lose")
+                Me.Close()
+            End If
+            startRocks()
+        End If
+
+    End Sub
+
+    Private Sub timerRock2_Tick(sender As Object, e As EventArgs) Handles timerRock2.Tick
+        rock2.Top = rock2.Top + 20
+        If rock2.Top >= 800 Then
+            rock2.Top = 0
+            speed = CInt(Int((200 * Rnd()) + 200))
+            timerRock2.Interval = speed
+        End If
+        If rock2.Bounds.IntersectsWith(pbxSpaceShip.Bounds) Then
+            timerRock1.Stop()
+            timerRock2.Stop()
+            timerRock3.Stop()
+            timerRock4.Stop()
+            timerRock5.Stop()
+            timerRock6.Stop()
+            timerRock7.Stop()
+            If lblLives.Text = "3" Then
+                MsgBox("You now have 2 lives remaining")
+                lblLives.Text = "2"
+            ElseIf lblLives.Text = "2" Then
+                MsgBox("You now have 1 life remaining")
+                lblLives.Text = "1"
+            ElseIf lblLives.Text = "1" Then
+                lblLives.Text = "0"
+                MsgBox("You Lose")
+                Me.Close()
+            End If
+            startRocks()
+        End If
+    End Sub
+
+    Private Sub timerRock3_Tick(sender As Object, e As EventArgs) Handles timerRock3.Tick
+        rock3.Top = rock3.Top + 20
+        If rock3.Top >= 800 Then
+            rock3.Top = 0
+            speed = CInt(Int((200 * Rnd()) + 200))
+            timerRock3.Interval = speed
+        End If
+        If rock3.Bounds.IntersectsWith(pbxSpaceShip.Bounds) Then
+            timerRock1.Stop()
+            timerRock2.Stop()
+            timerRock3.Stop()
+            timerRock4.Stop()
+            timerRock5.Stop()
+            timerRock6.Stop()
+            timerRock7.Stop()
+            If lblLives.Text = "3" Then
+                MsgBox("You now have 2 lives remaining")
+                lblLives.Text = "2"
+            ElseIf lblLives.Text = "2" Then
+                MsgBox("You now have 1 life remaining")
+                lblLives.Text = "1"
+            ElseIf lblLives.Text = "1" Then
+                lblLives.Text = "0"
+                MsgBox("You Lose")
+                Me.Close()
+            End If
+            startRocks()
+        End If
+    End Sub
+
+    Private Sub timerRock4_Tick(sender As Object, e As EventArgs) Handles timerRock4.Tick
+        rock4.Top = rock4.Top + 20
+        If rock4.Top >= 800 Then
+            rock4.Top = 0
+            speed = CInt(Int((200 * Rnd()) + 200))
+            timerRock4.Interval = speed
+        End If
+        If rock4.Bounds.IntersectsWith(pbxSpaceShip.Bounds) Then
+            timerRock1.Stop()
+            timerRock2.Stop()
+            timerRock3.Stop()
+            timerRock4.Stop()
+            timerRock5.Stop()
+            timerRock6.Stop()
+            timerRock7.Stop()
+            If lblLives.Text = "3" Then
+                MsgBox("You now have 2 lives remaining")
+                lblLives.Text = "2"
+            ElseIf lblLives.Text = "2" Then
+                MsgBox("You now have 1 life remaining")
+                lblLives.Text = "1"
+            ElseIf lblLives.Text = "1" Then
+                lblLives.Text = "0"
+                MsgBox("You Lose")
+                Me.Close()
+            End If
+            startRocks()
+        End If
+    End Sub
+
+    Private Sub timerRock5_Tick(sender As Object, e As EventArgs) Handles timerRock5.Tick
+        rock5.Top = rock5.Top + 20
+        If rock5.Top >= 800 Then
+            rock5.Top = 0
+            speed = CInt(Int((200 * Rnd()) + 200))
+            timerRock5.Interval = speed
+        End If
+        If rock5.Bounds.IntersectsWith(pbxSpaceShip.Bounds) Then
+            timerRock1.Stop()
+            timerRock2.Stop()
+            timerRock3.Stop()
+            timerRock4.Stop()
+            timerRock5.Stop()
+            timerRock6.Stop()
+            timerRock7.Stop()
+            If lblLives.Text = "3" Then
+                MsgBox("You now have 2 lives remaining")
+                lblLives.Text = "2"
+            ElseIf lblLives.Text = "2" Then
+                MsgBox("You now have 1 life remaining")
+                lblLives.Text = "1"
+            ElseIf lblLives.Text = "1" Then
+                lblLives.Text = "0"
+                MsgBox("You Lose")
+                Me.Close()
+            End If
+            startRocks()
+        End If
+    End Sub
+
+    Private Sub timerRock6_Tick(sender As Object, e As EventArgs) Handles timerRock6.Tick
+        rock6.Top = rock6.Top + 20
+        If rock6.Top >= 800 Then
+            rock6.Top = 0
+            speed = CInt(Int((200 * Rnd()) + 200))
+            timerRock6.Interval = speed
+        End If
+        If rock6.Bounds.IntersectsWith(pbxSpaceShip.Bounds) Then
+            timerRock1.Stop()
+            timerRock2.Stop()
+            timerRock3.Stop()
+            timerRock4.Stop()
+            timerRock5.Stop()
+            timerRock6.Stop()
+            timerRock7.Stop()
+            If lblLives.Text = "3" Then
+                MsgBox("You now have 2 lives remaining")
+                lblLives.Text = "2"
+            ElseIf lblLives.Text = "2" Then
+                MsgBox("You now have 1 life remaining")
+                lblLives.Text = "1"
+            ElseIf lblLives.Text = "1" Then
+                lblLives.Text = "0"
+                MsgBox("You Lose")
+                Me.Close()
+            End If
+            startRocks()
+        End If
+    End Sub
+
+    Private Sub timerRock7_Tick(sender As Object, e As EventArgs) Handles timerRock7.Tick
+        rock7.Top = rock7.Top + 20
+        If rock7.Top >= 800 Then
+            rock7.Top = 0
+            speed = CInt(Int((200 * Rnd()) + 200))
+            timerRock7.Interval = speed
+        End If
+        If rock7.Bounds.IntersectsWith(pbxSpaceShip.Bounds) Then
+            timerRock1.Stop()
+            timerRock2.Stop()
+            timerRock3.Stop()
+            timerRock4.Stop()
+            timerRock5.Stop()
+            timerRock6.Stop()
+            timerRock7.Stop()
+            If lblLives.Text = "3" Then
+                MsgBox("You now have 2 lives remaining")
+                lblLives.Text = "2"
+            ElseIf lblLives.Text = "2" Then
+                MsgBox("You now have 1 life remaining")
+                lblLives.Text = "1"
+            ElseIf lblLives.Text = "1" Then
+                lblLives.Text = "0"
+                MsgBox("You Lose")
+                Me.Close()
+            End If
+            startRocks()
+        End If
+    End Sub
+End Class
